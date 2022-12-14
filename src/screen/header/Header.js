1d74d38747e6f5962c79936ccd8e9b35bb5527d2
Adrian Gil <adrian.m.gil@gmail.com>
2022-12-14 13:29:33 -0500

    Fix few more minor issues with widths

--- src/screen/header/Header.js
+++ src/screen/header/Header.js
@@ -8,15 +8,13 @@ const Header = () => {
     return (
         <div className="header-wrapper">
             <GGLogo showText={true} />
-            <table>
-                <tr>
-                    <td><label>SHOP</label></td>
-                    <td><label>CLUBS</label></td>
-                    <td><label>ACCESSORIES</label></td>
-                    <td><label>ABOUT</label></td>
-                    <td><label>CONTACT</label></td>
-                </tr>
-            </table>
+                <div className="header-topmenu">
+                    <label>SHOP</label>
+                    <label>CLUBS</label>
+                    <label>ACCESSORIES</label>
+                    <label>ABOUT</label>
+                    <label>CONTACT</label>
+                </div>
             <HeaderAccount />
         </div>
     );
